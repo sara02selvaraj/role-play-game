96a789f66e2009b776969355071353dd91477c5c
saranya <selvaraj.umasaranya@gmail.com>
2022-03-27 12:35:01 -0400

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const data = {
+    hero: {
+        elementId: "hero",
+        name: "Wizard",
+        avatar: "images/wizard.png",
+        health: 60,
+        diceCount: 3
+    },
+    monstor: {
+        elementId: "monster",
+        name: "Orc",
+        avatar: "images/orc.png",
+        health: 10,
+        diceCount: 1
+    }
+}
