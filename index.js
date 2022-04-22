30ea83e90ffe6dfd53afae98e5039f4bcd8ce09e
saranya <selvaraj.umasaranya@gmail.com>
2022-04-21 20:22:18 -0400

    commit

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ let monstersArray = ['monstor', 'demon', 'goblin']
 
 attackBtn.addEventListener('click', attack)
 
+function getNewMonster(){
+  const nextMonsterData = characterData[monstersArray.shift()]
+  return nextMonsterData ? new Character(nextMonsterData) : {}
+
+}    
+
 function endGame() {
-    const endMessage = wizard.life === 0 && orc.life === 0 ?
+    const endMessage = wizard.life === 0 && monster.life === 0 ?
     "No victors - all creatures are dead " :
     wizard.life > 0 ? "The Wizard Wins " :
-    "The Orc is Victorious "
+    "The monster is Victorious "
+
     const endEmoji = wizard.life > 0 ? '🔮' : '☠️'
     document.body.innerHTML = 
     `<div class="end-game">
@@ -19,28 +26,31 @@ function endGame() {
         <p class="end-emoji">${endEmoji}</p>
     </div>` 
 }
-function getNewMonster() {
-    const nextMonsterData = characterData[monstersArray.shift()]
-    return nextMonsterData ? new Character(nextMonsterData) : {}
-}
+
 
 function attack(){
     wizard.diceArrayHtml()
-    monstor.diceArrayHtml()
-    wizard.takeDamage(monstor.currentDiceScore)
-    monstor.takeDamage(wizard.currentDiceScore)
+    monster.diceArrayHtml()
+    wizard.takeDamage(monster.currentDiceScore)
+    monster.takeDamage(wizard.currentDiceScore)
     render()
-    if(wizard.dead || monstor.dead){
+    if(wizard.dead){
         endGame()
+    }else if(monster.dead){
+        if(monstersArray.length > 0){
+           monster = getNewMonster()
+           render()
+        }else{
+            endGame()
+        }
     }
 }
 
 const wizard = new Character(characterData.hero)
-const monstor = new Character(characterData.monstor)
-
+let monster = getNewMonster()
 
 function render(){
     document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
-    document.getElementById('monstor').innerHTML = monstor.getCharacterHtml()
+    document.getElementById('monstor').innerHTML = monster.getCharacterHtml()
 }
 render()
