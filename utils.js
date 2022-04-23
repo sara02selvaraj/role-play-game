789506c6e1b2231e80f036b8a358db1c696c2826
saranya <selvaraj.umasaranya@gmail.com>
2022-04-23 12:30:32 -0400

    commit

--- utils.js
+++ utils.js
@@ -1,13 +1,13 @@
 function generateRandom(diceCount){
-    return new Array(diceCount).fill(0).map(function(){
-        return Math.floor(Math.random() *6)+1
-    })
+    return new Array(diceCount).fill(0).map(() => 
+         Math.floor(Math.random() *6)+1
+    )
 }
 
 function getDicePlaceholder(diceCount) {
-    return new Array(diceCount).fill(0).map(function(){
-        return `<div class="placeholder-dice"></div>`
-    }).join('')
+    return new Array(diceCount).fill(0).map(() => 
+         `<div class="placeholder-dice"></div>`
+    ).join('')
 }
 
 function calculateTotal(arr){
